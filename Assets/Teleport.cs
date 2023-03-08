bb554cc12d6a2a7bdc6f6e3eabd0696364457fa2
uptownrat <amyhoangvan@gmail.com>
2023-03-07 23:45:30 -0500

    added teleport and light

--- Assets/Teleport.cs
+++ Assets/Teleport.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CrystalInteract : MonoBehaviour
+public class Teleport : MonoBehaviour
 {
+    // Start is called before the first frame update
+    public GameObject destination;
     bool canInteract;
-    public Light crystal;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
             canInteract = true;
-            //Debug.Log("i'm in");
         }
     }
 
@@ -21,30 +21,21 @@ public class CrystalInteract : MonoBehaviour
         if (collision.tag == "Player")
         {
             canInteract = false;
-            //Debug.Log("i'm out");
         }
     }
 
-    // Start is called before the first frame update
     void Start()
     {
-        canInteract = false;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        // turn light on and off
         if (canInteract == true && Input.GetKeyDown(KeyCode.Z))
         {
-            if(crystal.intensity == 0)
-            {
-                crystal.intensity = 1;
-            }
-            else
-            {
-                crystal.intensity = 0;
-            }
+            GameObject.FindGameObjectWithTag("Player").transform.position = destination.transform.position;
+
         }
     }
 }
