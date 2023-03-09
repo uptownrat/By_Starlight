7eecb24141f88ad929956018991e5a7161aa8d77
uptownrat <amyhoangvan@gmail.com>
2023-03-09 16:35:29 -0500

    attempt to fix load scene

--- Assets/LoadNextScene.cs
+++ Assets/LoadNextScene.cs
@@ -13,7 +13,7 @@ public class LoadNextScene : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Scene currentScene = SceneManager.GetActiveScene();
+        //Scene currentScene = SceneManager.GetActiveScene();
 
         if(collision.gameObject.tag == "Player")
         {
